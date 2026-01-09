f448685ac93bb6fa5fd99d801e1dbe414db47b31
nhan nguyen <resido.ken@gmail.com>
2026-01-09 11:56:14 +0700

    fixed the "400 Bad Request" error

--- apps/api/Program.cs
+++ apps/api/Program.cs
@@ -82,7 +82,11 @@ builder.Services.AddCors(options =>
 // ============================================
 // Controllers + Swagger
 // ============================================
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
